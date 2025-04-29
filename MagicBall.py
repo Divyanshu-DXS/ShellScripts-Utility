73a81a26821067e9a76dbb3738259c1eb55aa17e
Divyanshu Sharma (Dev) <165436585+Divyanshu-DXS@users.noreply.github.com>
2025-04-29 01:53:12 -0400

    Magic 8 Ball
    Magic 8 Ball

---
+++ MagicBall.py
@@ -0,0 +1,14 @@
+print("This is the magic ball !! Welcome")
+print("ADVICE OF THE DAY FOR YOU !!//\n\n")
+result = ['You have got this. Believe in yourself',
+          'Get shit done',
+          'Little steps. bigger victories',
+          'Dont think about failures, think about What you got done!',
+          'Life ahead is a ride to enjoy',
+          'God has got your back ',
+          'Think Emotionally today, give your heart a chance',
+          'Step Carefully.']
+
+import random
+print(result[random.randint(0,len(result))])
+print()
